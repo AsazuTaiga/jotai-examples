35905b497ffbfb1f4f8e80b28466bb26403da5dc
Asazu Taiga <taiga.asazu@icloud.com>
2023-03-21 02:00:41 +0900

    fixxxx

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // github pages
-  base: "/jotai-examples/",
+  base: "/jotai-examples/dist/",
   plugins: [react()],
 });
